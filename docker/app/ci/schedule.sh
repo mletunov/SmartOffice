9b2657c7db266e664a2f5ebaec1e1573cc646b27
mletunov <maxim.letunov@artezio.com>
2017-03-17 02:30:05 +0300

    build scripts - setup environment variables and add more logs

--- docker/app/ci/schedule.sh
+++ docker/app/ci/schedule.sh
@@ -5,6 +5,9 @@ workspace=/var/cron
 root=$workspace/SmartOffice
 log=/var/cron/log
 
+export SHELL=/bin/sh
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
 if [ ! -d $root ]; then
     echo "`date`: Git clone " >> $log
     mkdir -p $workspace && cd $workspace
@@ -17,5 +20,5 @@ fi
 
 if [ -n "$changes" ]; then
     # build web
-    $root/docker/app/web/build.sh `pwd`
+    chmod +x $root/docker/app/web/build.sh && sync && $root/docker/app/web/build.sh $root $log
 fi
--- docker/app/web/build.sh
+++ docker/app/web/build.sh
@@ -2,16 +2,20 @@
 
 
 root=$1
+log=$2
 
+echo "`date`: Building web" >> $log
 cd $root/web && \
-npm install && npm run build && \
+npm install 2>&1 | tee -a $log && npm run build 2>&1 | tee -a $log && \
 rm $root/docker/app/web/build -rf && cp -a build $root/docker/app/web/build
 
-docker build -t app $root/docker/app/web && \
+echo "`date`: Building docker image" >> $log
+docker build -t app $root/docker/app/web 2>&1 | tee -a $log && \
 read container_id image_id <<< $(docker ps -a | grep web | awk '{print $1" "$2}')
 
 if [[ $image_id =~ [0-9a-f]+$ ]] ; then
-   echo "remove old container and image"
+   echo "`date`: Remove old container $container_id and image $image_id" >> $log
    docker rm -f $container_id && docker rmi -f $image_id && \
    docker run -d -p 80:80 -p 443:443 --restart always --name web app
+   echo "`date`: Container 'web' has been successfully run" >> $log
 fi
