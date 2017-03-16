6eda489e75c7afd8b9b5342ad4d05da4d226cee4
Maxim Letunov <maxim.letunov@artezio.com>
2017-03-16 10:56:35 +0300

    remove useless node image

--- docker/base/node/node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-PREFIX="usr/local/"
-
-mkdir -p "$PREFIX" \
-&& wget -qO- $1 | tar xvJ --strip-components=1 -C "$PREFIX" \
-&& npm config set unsafe-perm true -g --unsafe-perm \
-&& npm install webpack-dev-server -g
