222316a9db1d543d13607d7092d6bbc5ba6d1019
oshepotkin <oleg.shepotkin@artezio.com>
2017-03-17 16:34:02 +0300

    fix itnl issue

--- web/src/intl/IntlReducer.js
+++ web/src/intl/IntlReducer.js
@@ -4,10 +4,20 @@ import { SWITCH_LANGUAGE } from './IntlActions';
 const initLocale = global.navigator && global.navigator.language || 'en';
 
 const initialState = {
-  locale: initLocale,
+  locale: get_language(initLocale),
   enabledLanguages,
-  ...(localizationData[initLocale] || {}),
+  ...(localizationData[get_language(initLocale)] || {}),
 };
+
+function get_language(locale)
+{
+    const dash_index = locale.indexOf('-')
+    if (dash_index >= 0)
+    {
+        return locale.substring(0, dash_index)
+    }
+    return locale
+}
 
 const IntlReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -15,7 +25,6 @@ const IntlReducer = (state = initialState, action) => {
       const { type, ...actionWithoutType } = action; // eslint-disable-line
       return { ...state, ...actionWithoutType };
     }
-
     default:
       return state;
   }
